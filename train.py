faeaafe2c02f8658c6bfe5c7b6f541d162afb090
carlotasantos <149483188+carlotasantos@users.noreply.github.com>
2025-12-27 17:30:12 +0000

    No train.py foi implementado o treino e a avalição de CNN no MNIST e no model.py  implemento a CNN base para o MNIST

---
+++ train.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from model import MNISTCNN
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Device:", device)
+
+# carregar MNIST
+transform = transforms.ToTensor()
+train_ds = datasets.MNIST(root="data", train=True, download=False, transform=transform)
+test_ds  = datasets.MNIST(root="data", train=False, download=False, transform=transform)
+
+train_loader = DataLoader(train_ds, batch_size=64, shuffle=True)
+test_loader  = DataLoader(test_ds, batch_size=256, shuffle=False)
+
+# modelo + loss + otimizador
+model = MNISTCNN().to(device)
+loss_fn = nn.CrossEntropyLoss()
+opt = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+epochs = 2
+
+for epoch in range(epochs):
+    # treino
+    model.train()
+    for x, y in train_loader:
+        x, y = x.to(device), y.to(device)
+        opt.zero_grad()
+        out = model(x)
+        loss = loss_fn(out, y)
+        loss.backward()
+        opt.step()
+
+    # accuracy
+    model.eval()
+    correct, total = 0, 0
+    with torch.no_grad():
+        for x, y in test_loader:
+            x, y = x.to(device), y.to(device)
+            out = model(x)
+            pred = out.argmax(1)
+            correct += (pred == y).sum().item()
+            total += y.size(0)
+
+    acc = correct / total
+    print(f"Epoch {epoch+1}/{epochs} | test_acc={acc:.4f}")
+
+
+torch.save(model.state_dict(), "mnist_cnn.pt")
+print("Modelo guardado em: mnist_cnn.pt")
+
