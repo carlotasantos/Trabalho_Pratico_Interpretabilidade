4aac8bbb4d513ab54ce4ced2f347a4a44dd94b9a
carlotasantos <149483188+carlotasantos@users.noreply.github.com>
2025-12-27 17:33:45 +0000

    agora o main carrega o modelo treinado e prevê 1 imagem

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from torchvision import datasets
-from metrics_utils import gt_box_mnist
+import torch
+from torchvision import datasets, transforms
 
-# carregar dataset (já descarregado)
-train = datasets.MNIST(root="data", train=True, download=False)
+from model import MNISTCNN
 
-img, label = train[0]
-box = gt_box_mnist(img)
 
-print("Label:", label)
-print("GT box:", box)
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Device:", device)
+
+    transform = transforms.ToTensor()
+    train = datasets.MNIST(root="data", train=True, download=False, transform=transform)
+
+    x, y_true = train[0]
+    x = x.unsqueeze(0).to(device)
+
+    # carrega o  modelo treinado
+    model = MNISTCNN().to(device)
+    model.load_state_dict(torch.load("mnist_cnn.pt", map_location=device))
+    model.eval()
+
+    # predição
+    with torch.no_grad():
+        logits = model(x)
+        y_pred = logits.argmax(1).item()
+
+    print("Label verdadeira:", y_true)
+    print("Predição do modelo:", y_pred)
+
+
+if __name__ == "__main__":
+    main()
