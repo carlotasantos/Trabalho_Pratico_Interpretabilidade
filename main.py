e2c14fb6504278909b2ac85572383fc5dad2380a
carlotasantos <149483188+carlotasantos@users.noreply.github.com>
2025-12-27 17:57:05 +0000

    alterei o main para substituir a parte do exemplo por um ciclo

--- main.py
+++ main.py
@@ -1,53 +1,54 @@
 import torch
 from torchvision import datasets, transforms
-from model import MNISTCNN
-
 from metrics_utils import gt_box_mnist, pointing_game_hit
-
+from model import MNISTCNN
 
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Device:", device)
 
+    N = 200  # nº de imagens que avalia
+
     transform = transforms.ToTensor()
 
-    train_pil = datasets.MNIST(root="data", train=True, download=False)  # sem transform
-    img_pil, y_true = train_pil[0]
+    # dataset para bbox
+    train_pil = datasets.MNIST(root="data", train=True, download=False)
 
+    # dataset para o modelo
     train_tensor = datasets.MNIST(root="data", train=True, download=False, transform=transform)
-    x, _ = train_tensor[0]
-    x = x.unsqueeze(0).to(device)
 
+    # modelo treinado
     model = MNISTCNN().to(device)
     model.load_state_dict(torch.load("mnist_cnn.pt", map_location=device))
     model.eval()
 
+    hits = 0
 
-    #predição
-    #permite gradientes na imagem
-    x.requires_grad_()
+    for i in range(N):
+        img_pil, y_true = train_pil[i]
+        x, _ = train_tensor[i]
+        x = x.unsqueeze(0).to(device)
 
-    logits = model(x)
-    y_pred = logits.argmax(1).item()
-    score = logits[0, y_pred]
+        # saliency por gradiente
+        x.requires_grad_()
+        logits = model(x)
+        y_pred = logits.argmax(1).item()
+        score = logits[0, y_pred]
 
-    model.zero_grad()
-    score.backward()
+        model.zero_grad()
+        score.backward()
 
-    # gradiente da imagem
-    saliency = x.grad.abs().squeeze().cpu().numpy()
+        saliency = x.grad.abs().squeeze().cpu().numpy()
 
-    gt_box = gt_box_mnist(img_pil)
-    hit = pointing_game_hit(saliency, gt_box)
+        # pointing game
+        gt_box = gt_box_mnist(img_pil)
+        hit = pointing_game_hit(saliency, gt_box)
+        hits += hit
 
+    score_pg = hits / N
+    print(f"Pointing Game score (N={N}): {score_pg:.4f}")
 
-    print("Label verdadeira:", y_true)
-    print("Predição do modelo:", y_pred)
-    print("Saliency shape:", saliency.shape)
-    print("Saliency min/max:", saliency.min(), saliency.max())
-    print("GT box:", gt_box)
-    print("Pointing Game hit:", hit)
 
 if __name__ == "__main__":
     main()
