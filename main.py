61c943c57852f34b9f19f3d73bee24d890732ffd
carlotasantos <149483188+carlotasantos@users.noreply.github.com>
2025-12-27 17:44:06 +0000

    gero a primeira explicação que é o Saliency Map por Gradient

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import torch
 from torchvision import datasets, transforms
-
 from model import MNISTCNN
+
+from metrics_utils import gt_box_mnist, pointing_game_hit
+
 
 
 def main():
@@ -11,21 +13,37 @@ def main():
     transform = transforms.ToTensor()
     train = datasets.MNIST(root="data", train=True, download=False, transform=transform)
 
-    x, y_true = train[0]
+    train_pil = datasets.MNIST(root="data", train=True, download=False)  # sem transform
+    img_pil, y_true = train_pil[0]
+
+    train_tensor = datasets.MNIST(root="data", train=True, download=False, transform=transform)
+    x, _ = train_tensor[0]
     x = x.unsqueeze(0).to(device)
 
-    # carrega o  modelo treinado
     model = MNISTCNN().to(device)
     model.load_state_dict(torch.load("mnist_cnn.pt", map_location=device))
     model.eval()
 
-    # predição
-    with torch.no_grad():
-        logits = model(x)
-        y_pred = logits.argmax(1).item()
+    x.requires_grad_()
 
-    print("Label verdadeira:", y_true)
-    print("Predição do modelo:", y_pred)
+    logits = model(x)
+    y_pred = logits.argmax(1).item()
+    score = logits[0, y_pred]
+
+    model.zero_grad()
+    score.backward()
+
+    # gradiente da imagem
+    saliency = x.grad.abs().squeeze().cpu().numpy()
+
+    gt_box = gt_box_mnist(img_pil)
+    hit = pointing_game_hit(saliency, gt_box)
+
+    print("GT box:", gt_box)
+    print("Pointing Game hit:", hit)
+
+    print("Saliency shape:", saliency.shape)
+    print("Saliency min/max:", saliency.min(), saliency.max())
 
 
 if __name__ == "__main__":
